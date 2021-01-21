f821e09178c1aff2d94e824bcb839d80198b1b42
jwchan <741755613@qq.com>
2021-01-21 16:47:19 +0800

    feat: 展示文章详情以及处理markdown显示

--- pages/article/index.vue
+++ pages/article/index.vue
@@ -2,7 +2,8 @@
   <div class="article-page">
     <div class="banner">
       <div class="container">
-        <h1>How to build webapps that scale</h1>
+
+        <h1>{{ article.title }}</h1>
 
         <div class="article-meta">
           <a href=""><img src="http://i.imgur.com/Qr71crq.jpg" /></a>
@@ -25,14 +26,7 @@
 
     <div class="container page">
       <div class="row article-content">
-        <div class="col-md-12">
-          <p>
-            Web development technologies have evolved at an incredible clip over
-            the past few years.
-          </p>
-          <h2 id="introducing-ionic">Introducing RealWorld.</h2>
-          <p>It's a great solution for learning how other frameworks work.</p>
-        </div>
+        <div class="col-md-12" v-html="article.body"></div>
       </div>
 
       <hr />
@@ -128,8 +122,23 @@
 </template>
 
 <script>
+import { getArticle } from '@/api/article'
+import MarkdownIt from 'markdown-it'
+
 export default {
-  name: 'ArticleIndex'
+  name: 'ArticleIndex',
+  async asyncData ({ params }) {
+    const { data } = await getArticle(params.slug)
+
+    const { article } = data
+    // markdown => html
+    const md = new MarkdownIt()
+    article.body = md.render(article.body)
+
+    return { 
+      article
+    }
+  }
 };
 </script>
 
