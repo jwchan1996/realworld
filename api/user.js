a05ef5c3bdc66f5ad0ecddf8749c90d3ffe7a693
jwchan <741755613@qq.com>
2021-01-21 17:15:03 +0800

    feat: 展示文章作者相关信息

--- api/user.js
+++ api/user.js
@@ -17,3 +17,19 @@ export function register(data) {
     data
   })
 }
+
+// 关注用户
+export function addFollow (username) {
+  return request({
+    method: 'POST',
+    url: `/api/profiles/${username}/follow`
+  })
+}
+
+// 取消关注
+export function deleteFollow (username) {
+  return request({
+    method: 'DELETE',
+    url: `/api/profiles/${username}/follow`
+  })
+}
--- pages/article/index.vue
+++ pages/article/index.vue
@@ -5,22 +5,8 @@
 
         <h1>{{ article.title }}</h1>
 
-        <div class="article-meta">
-          <a href=""><img src="http://i.imgur.com/Qr71crq.jpg" /></a>
-          <div class="info">
-            <a href="" class="author">Eric Simons</a>
-            <span class="date">January 20th</span>
-          </div>
-          <button class="btn btn-sm btn-outline-secondary">
-            <i class="ion-plus-round"></i>
-            &nbsp; Follow Eric Simons <span class="counter">(10)</span>
-          </button>
-          &nbsp;&nbsp;
-          <button class="btn btn-sm btn-outline-primary">
-            <i class="ion-heart"></i>
-            &nbsp; Favorite Post <span class="counter">(29)</span>
-          </button>
-        </div>
+        <article-meta :article="article"></article-meta>
+
       </div>
     </div>
 
@@ -32,25 +18,9 @@
       <hr />
 
       <div class="article-actions">
-        <div class="article-meta">
-          <a href="profile.html"
-            ><img src="http://i.imgur.com/Qr71crq.jpg"
-          /></a>
-          <div class="info">
-            <a href="" class="author">Eric Simons</a>
-            <span class="date">January 20th</span>
-          </div>
+        
+        <article-meta :article="article"></article-meta>
 
-          <button class="btn btn-sm btn-outline-secondary">
-            <i class="ion-plus-round"></i>
-            &nbsp; Follow Eric Simons <span class="counter">(10)</span>
-          </button>
-          &nbsp;
-          <button class="btn btn-sm btn-outline-primary">
-            <i class="ion-heart"></i>
-            &nbsp; Favorite Post <span class="counter">(29)</span>
-          </button>
-        </div>
       </div>
 
       <div class="row">
@@ -124,6 +94,7 @@
 <script>
 import { getArticle } from '@/api/article'
 import MarkdownIt from 'markdown-it'
+import ArticleMeta from './components/article-meta.vue'
 
 export default {
   name: 'ArticleIndex',
@@ -138,6 +109,9 @@ export default {
     return { 
       article
     }
+  },
+  components: {
+    ArticleMeta
   }
 };
 </script>
